5b328be5d4f64f81794544e0cd6b97ece3b475f6
Richard Andersen <randersenyb@andersenfamily.us>
2023-09-18 12:49:30 -0700

    Added "Show Password" CB

--- ConnectionDialog.xaml.cs
+++ ConnectionDialog.xaml.cs
@@ -59,7 +59,7 @@ namespace Aerospike.Database.LINQPadDriver
                 }
 
                 cbUsePassMgr_Click(this.cbUsePassMgr, new RoutedEventArgs());
-            }            
+            }
         }
         
         void btnOK_Click (object sender, RoutedEventArgs e)
@@ -368,6 +368,22 @@ Note: If the DB has Public/NATted/Alternate Addresses,
                 this.spPasswordNames.Visibility = Visibility.Hidden;                
             }
         }
+
+        private void cbShowPasswordChars_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordBox.Visibility = Visibility.Collapsed;
+            txtPassword.Visibility = Visibility.Visible;
+
+            txtPassword.Focus();
+        }
+
+        private void cbShowPasswordChars_Unchecked(object sender, RoutedEventArgs e)
+        {
+            txtPassword.Visibility = Visibility.Collapsed;
+            passwordBox.Visibility = Visibility.Visible;
+
+            passwordBox.Focus();
+        }        
     }
 
     public class NumericValidationRule : ValidationRule
@@ -453,5 +469,92 @@ Note: If the DB has Public/NATted/Alternate Addresses,
         }
 
         #endregion
+    }
+
+    public static class PasswordHelper
+    {
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.RegisterAttached("Password",
+            typeof(string), typeof(PasswordHelper),
+            new FrameworkPropertyMetadata(string.Empty, OnPasswordPropertyChanged));
+
+        public static readonly DependencyProperty AttachProperty =
+            DependencyProperty.RegisterAttached("Attach",
+            typeof(bool), typeof(PasswordHelper), new PropertyMetadata(false, Attach));
+
+        private static readonly DependencyProperty IsUpdatingProperty =
+           DependencyProperty.RegisterAttached("IsUpdating", typeof(bool),
+           typeof(PasswordHelper));
+
+
+        public static void SetAttach(DependencyObject dp, bool value)
+        {
+            dp.SetValue(AttachProperty, value);
+        }
+
+        public static bool GetAttach(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(AttachProperty);
+        }
+
+        public static string GetPassword(DependencyObject dp)
+        {
+            return (string)dp.GetValue(PasswordProperty);
+        }
+
+        public static void SetPassword(DependencyObject dp, string value)
+        {
+            dp.SetValue(PasswordProperty, value);
+        }
+        
+        private static bool GetIsUpdating(DependencyObject dp)
+        {
+            return (bool)dp.GetValue(IsUpdatingProperty);
+        }
+
+        private static void SetIsUpdating(DependencyObject dp, bool value)
+        {
+            dp.SetValue(IsUpdatingProperty, value);
+        }
+
+        private static void OnPasswordPropertyChanged(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            passwordBox.PasswordChanged -= PasswordChanged;
+
+            if (!(bool)GetIsUpdating(passwordBox))
+            {
+                passwordBox.Password = (string)e.NewValue;
+            }
+            passwordBox.PasswordChanged += PasswordChanged;
+        }
+
+        private static void Attach(DependencyObject sender,
+            DependencyPropertyChangedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+
+            if (passwordBox == null)
+                return;
+
+            if ((bool)e.OldValue)
+            {
+                passwordBox.PasswordChanged -= PasswordChanged;
+            }
+
+            if ((bool)e.NewValue)
+            {
+                passwordBox.PasswordChanged += PasswordChanged;
+            }
+        }
+
+        private static void PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            PasswordBox passwordBox = sender as PasswordBox;
+            SetIsUpdating(passwordBox, true);
+            SetPassword(passwordBox, passwordBox.Password);
+            SetIsUpdating(passwordBox, false);            
+        }
     }
 }
