82a6380de55e18febddff4319bf62523e787395a
Richard Andersen <randersenyb@andersenfamily.us>
2025-07-11 18:10:35 -0700

    Updated to support the MacOS NuGet format

---
+++ NuGetUpdPkgStruct/PkgUpdate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO.Compression;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NuGetUpdPkgStruct
+{
+	internal static class PkgUpdate
+	{
+		public static void Apply(string nugetFilePath,
+									string regexFrameworkStr,
+									string regexPkgNameStr)
+		{
+			var frameworkRegEx = new Regex(regexFrameworkStr,
+											RegexOptions.Singleline
+												| RegexOptions.IgnoreCase
+												| RegexOptions.Compiled);
+			
+			using(ZipArchive archive = ZipFile.Open(nugetFilePath, ZipArchiveMode.Update))
+			{
+				var frameworkNetList = new List<string>();
+				var pkgMatch = Regex.Match(Path.GetFileNameWithoutExtension(nugetFilePath),
+											regexPkgNameStr);
+
+				var nuspecEntry = pkgMatch.Success
+									? archive.GetEntry(pkgMatch.Groups["pkgname"].Value + ".nuspec")
+									: null;
+
+				// Iterate through all entries in the archive		
+				foreach(ZipArchiveEntry entry in archive.Entries.ToArray())
+				{
+					// Check if the entry's full name matches the framework for windows
+					var match = frameworkRegEx.Match(entry.FullName);
+
+					if(match.Success)
+					{
+						// Construct the new full name by replacing the old folder name with the new one
+						string newFullName = entry.FullName.Replace(match.Groups["framework"].Value, match.Groups["netver"].Value);
+
+						// Create a new entry with the updated name
+						ZipArchiveEntry newEntry = archive.CreateEntry(newFullName);
+
+						// Copy the content from the old entry to the new entry
+						using(Stream oldStream = entry.Open())
+						using(Stream newStream = newEntry.Open())
+						{
+							oldStream.CopyTo(newStream);
+						}
+
+						// Delete the old entry
+						entry.Delete();
+
+						//update nuspec file, if not already updated
+						if(nuspecEntry != null && !frameworkNetList.Contains(match.Groups["framework"].Value))
+						{
+							StringBuilder nuspecDoc;
+
+							using(var readStream = nuspecEntry.Open())
+							using(StreamReader reader = new StreamReader(readStream))
+							{
+								nuspecDoc = new StringBuilder(reader.ReadToEnd());
+							}
+
+							nuspecEntry.Delete();
+							var newNuspecEntry = archive.CreateEntry(nuspecEntry.FullName);
+							nuspecEntry = newNuspecEntry;
+							nuspecDoc.Replace(match.Groups["framework"].Value, match.Groups["netver"].Value);
+
+							using(StreamWriter writer = new StreamWriter(newNuspecEntry.Open()))
+							{
+								writer.Write(nuspecDoc);
+							}
+						}
+						frameworkNetList.Add(match.Groups["framework"].Value);
+					}
+				}
+			}
+		}
+	}
+}
---
+++ NuGetUpdPkgStruct/Program.cs
@@ -0,0 +1,129 @@
+﻿
+using System.CommandLine;
+
+namespace NuGetUpdPkgStruct;
+
+public class Program
+{
+	[System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA2208:Instantiate argument exceptions correctly", Justification = "<Pending>")]
+	static int Main(string[] args)
+	{
+		RootCommand rootCommand = new("This program will update a NuGet Package so that it can support LINQPad drivers on MacOS.")
+		{
+			new Argument<FileInfo> ("package")
+			{
+				Description = "The NuGet File Package Path",
+				HelpName = "NuGet File Package Path"
+			},
+			new Option<FileInfo?>("--newpkg", "-n")
+			{
+				Description = "If provided, the updates will be saved to this NuGet File Package"
+			},
+			new Option<string> ("--regexFramework")
+			{
+				Description = @"This regex is used to parse the folder names looking to change based on the RegEx.
+The RegEx must define a group named ""framework"" which is the name that will be replaced and ""netver"" is the new name.
+Example: lib/net6.0-windows7.0/Aerospike.Database.LINQPadDriver.dll => lib/net6.0/Aerospike.Database.LINQPadDriver.dll",
+				Required = false,
+				DefaultValueFactory = argResult => @"(?<framework>(?<netver>net\d+\.\d+)\-windows\d+\.\d+)"
+			},
+			new Option<string> ("--regexPkgName")
+			{
+				Description = @"This regex is used to parse the NuGet File Path to obtain the NuGet Package name.
+The RegEx must define a group named ""pkgname"" which is the NuGet Package.
+Example: E:\Aerospike.Database.LINQPadDriver.6.0.5.nupkg => Aerospike.Database.LINQPadDriver",
+				Required = false,
+				DefaultValueFactory = argResult => @"(?<pkgname>.+)\.\d+\.\d+\.\d+"
+			}
+		};
+
+		rootCommand.SetAction(static parseResult =>
+		{
+			var nuGetFile = parseResult.GetValue<FileInfo>("package");
+			if(nuGetFile is null || !nuGetFile.Exists)
+			{
+				if(nuGetFile is null)
+					throw new ArgumentNullException("package", "Must supply a NuGet Path File. none Provided.");
+
+				throw new FileNotFoundException($"NuGet File \"{nuGetFile.FullName}\" was not found");
+			}
+			var nuGetNewFile = parseResult.GetValue<FileInfo?>("--newpkg");
+			var regexFramework = parseResult.GetValue<string>("--regexFramework");
+			var regexPkgname = parseResult.GetValue<string>("--regexPkgName");
+
+			if(string.IsNullOrEmpty(regexFramework))
+				throw new ArgumentNullException("--regexFramework", "Must supply a RegEx string.");
+			if(string.IsNullOrEmpty(regexPkgname))
+				throw new ArgumentNullException("--regexPkgName", "Must supply a RegEx string.");
+
+			string? tempFilename = null;
+
+			try
+			{
+				if(nuGetNewFile != null)
+				{
+					if(nuGetNewFile.Exists)
+					{
+						tempFilename = CopyFileToTemp(nuGetNewFile.FullName);
+					}
+					File.Copy(nuGetFile.FullName, nuGetNewFile.FullName, true);
+					nuGetFile = nuGetNewFile;
+				}
+
+				PkgUpdate.Apply(nuGetFile.FullName,
+									regexFramework,
+									regexPkgname);
+			}
+			catch(Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.GetType().Name} Message: {ex.Message}");
+
+				if(nuGetNewFile != null && tempFilename != null && File.Exists(tempFilename))
+				{
+					File.Copy(tempFilename, nuGetNewFile.FullName, true);
+				}
+				Console.WriteLine($"NuGet Package Update Failed for \"{nuGetFile}\"");
+				return -1;
+			}
+			finally
+			{
+				if(tempFilename != null && File.Exists(tempFilename))
+				{
+					File.Delete(tempFilename);
+				}
+			}
+
+			Console.WriteLine($"NuGet Package Update Completed for \"{nuGetFile}\"");
+			return 0;
+		});
+
+		ParseResult parseResult = rootCommand.Parse(args);
+		return parseResult.Invoke();
+	}
+
+	public static string? CopyFileToTemp(string sourceFilePath)
+	{
+		// Generate a unique temporary file path
+		string? tempFilePath = Path.GetTempFileName();
+
+		try
+		{
+			// Copy the source file to the temporary file path
+			File.Copy(sourceFilePath, tempFilePath, true); // Overwrite if the temporary file already exists
+		}
+		catch(FileNotFoundException)
+		{
+			tempFilePath = null;
+			Console.WriteLine($"Error: Source file not found: {sourceFilePath}");
+		}
+		catch(Exception)
+		{
+			tempFilePath = null;
+			throw;
+		}
+		return tempFilePath;
+	}
+}
+
+
+
